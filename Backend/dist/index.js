e3281ec31cd76157b94353299704e16ee167e708
Max Lell <maximilian.lell@gmail.com>
2023-11-06 08:27:39 +0000

    got the template code for the project up and running

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,10 +1,7 @@
 import express from 'express';
-
 const app = express();
-
 // Middlewares
 app.use(express.json());
-
 // Connections and listener
-
 app.listen(10000, () => console.log('server open'));
+//# sourceMappingURL=index.js.map
