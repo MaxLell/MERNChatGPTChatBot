4f154b9f85ecf33a622a98e8353156317ae4c1a8
Max Lell <maximilian.lell@gmail.com>
2023-11-12 18:39:44 +0000

    Added middleware for the userSignup with express-validators

--- Backend/src/controllers/user-controllers.ts
+++ Backend/src/controllers/user-controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/User.js';
+import { hash } from 'bcrypt';
 
 /**
  * Get all users from the Database
@@ -16,7 +17,39 @@ export const getAllUsers = async (
   } catch (error) {
     console.log(error);
     return res
-      .status(200)
+      .status(418) // I am a Teapot
       .json({ message: 'ERROR', cause: error.message });
   }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Extract Details from the Request Body
+    const { name, email, password } = req.body;
+
+    // Create a hashed version of the Password to not work with clear-text passwords in the DB
+    const saltRounds = 10;
+    const hashedPassword = await hash(password, saltRounds);
+
+    // Create new user and save it in the DB
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    // return the status
+    console.log(user._id.toString());
+    return res.status(201).json({
+      message: 'OK',
+      id: user._id.toString(),
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(418) // I am a Teapot
+      .json({ message: 'ERRORSIS', cause: error.message });
+  }
+  console.log('userSignup');
 };
--- Backend/src/routes/user-routes.ts
+++ Backend/src/routes/user-routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/user-controllers.js';
+import {
+  getAllUsers,
+  userSignup,
+} from '../controllers/user-controllers.js';
+import { signUpValidator, validate } from '../utils/validators.js';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getAllUsers);
+userRoutes.post('/signup', validate(signUpValidator), userSignup);
 
 export default userRoutes;
